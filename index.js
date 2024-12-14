ca9640e18beb621546f15f821d4f4306df174c6d
Naftaly Ngeru <n.ngeru.k@gmail.com>
2024-12-14 21:53:36 +0300

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ['Naftaly', 'Katherine', 'Jeremy']
+
+function writeCards(array){
+    let newArray = []
+    for (let x = 0; x < array.length; x++) {
+        const element = array[x];
+        newArray.push(`Thank you, ${element}, for the wonderful surprise gift!`);
+    }
+    return newArray
+}
+
+console.log(writeCards(names));
+
+function countDown(i){
+    let array = [];
+    for (let x = i; x >= 0 ; x--) {
+        console.log(x);
+        array.push(x);        
+    }
+    return array;
+}
+
+console.log(countDown(5))
